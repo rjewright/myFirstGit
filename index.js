6016b07ed4dda85a2c27b35da09e235e9566c5a7
rjewright <60637032+rjewright@users.noreply.github.com>
2021-08-06 13:19:32 +1000

    added adding function called idk what im doing

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("hello world");
+
+function idontknowwhatimDOING(a,b) { //this function shows that i don't know what i'm doing
+    return a + b; 
+}
+
+console.log(idontknowwhatimDOING(5,10));
